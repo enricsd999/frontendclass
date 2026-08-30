328162aba014361e0bd9ae0c4795929e0abcbbd4
unknown <enricsd999@gmail.com>
2026-08-30 13:35:34 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import "./App.css";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
